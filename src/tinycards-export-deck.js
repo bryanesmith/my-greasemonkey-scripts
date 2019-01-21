1c170ba8f4625ca3caf5358dc925143556aaf691
Bryan Smith <bryanesmith@gmail.com>
2019-01-21 18:59:55 -0500

    instead of putting cards in console, render near top of page

--- src/tinycards-export-deck.js
+++ src/tinycards-export-deck.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Tinycards export cards from deck
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Exports Tinycard deck's cards as a CSV to your JS console. Copy & paste everything between the horizontal lines.
+// @version      1.1
+// @description  When on Tinycards deck "Cards" view, renders cards' contents in CSV format near top of page.
 // @author       Bryan Smith
 // @match        https://tinycards.duolingo.com/decks/*
 // @grant        none
@@ -13,11 +13,21 @@
 
     // The fragile stuff
     var delay = 3000;
+    var selContainer = "._3dvU9";
     var selCard = "._2QLEC";
     var selVal = "._2tErK";
     var HR = "---------------------------------------------------------------";
 
     var exportCards = function() {
+        var container = document.querySelector(selContainer);
+        if (!container) {
+            console.error('No cards found. You may need to refresh the page after selecting the "cards" view for a deck.');
+            return;
+        }
+        var csv = document.createElement("div");
+        csv.style.cssText = "height: 150px; border: 1px solid #aaa; background-color: #eee; padding: 1em; overflow: scroll; margin-bottom: 1em; border-radius: .5em;";
+        container.prepend(csv);
+
         var cards = document.querySelectorAll(selCard);
         console.log("Found " + cards.length + " cards.");
         console.log(HR);
@@ -26,7 +36,7 @@
         for(var i = 0; i < cards.length; i++) {
             var cardSides = cards[i].querySelectorAll(selVal);
             if (cardSides.length == 2) {
-                console.log('"'+cardSides[0].innerText+'","'+cardSides[1].innerText+'"');
+                csv.innerHTML += '"'+cardSides[0].innerText+'","'+cardSides[1].innerText+'"<br />';
             } else {
                 console.error("Expected two sides, found: " + cardSides.length);
             }
