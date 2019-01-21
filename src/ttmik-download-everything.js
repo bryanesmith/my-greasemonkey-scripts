7eac0131cd20203749e5734bc6e2421a3a377b2a
Bryan Smith <bryanesmith@gmail.com>
2019-01-21 17:58:49 -0500

    script for downloading everything on TTMIK downloads page

---
+++ src/ttmik-download-everything.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         TTMIK Download Everything
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Download everything with a reasonable name
+// @author       You
+// @match        https://www.talktomeinkorean.com/mypage/downloads
+// @grant        GM_download
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.onload = function() {
+        setTimeout(function() {
+            console.log("Downloading everything from your TTMIK downloads page");
+            var downloads = document.querySelectorAll("a[href$=mp3]");
+            for(var i = 0; i < downloads.length; i++) {
+                var name = downloads[i].previousSibling.innerText;
+                // console.log("Would have downloaded: " + name);
+                GM_download({
+                    name: name,
+                    url: downloads[i].href,
+                    onload: function(r) {
+                        console.error("Success, downloaded "+name);
+                    },
+                    onerror: function(r) {
+                        console.error("Failed to download "+name);
+                    }
+                });
+            }
+        }, 5000);
+    };
+
+})();
